398e34bee2e4ac41272b943acb0a732cb0ae8268
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2018-02-27 20:26:14 +0100

    all solutions

--- session1/index_start.js
+++ session1/index_start.js
@@ -22,31 +22,67 @@ return str === str.split('').reverse().join('');
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int) {}
+function reverseInt(int) {
+  // checking if positive
+  const isPositive = int > 0;
+  // reversing int
+  const tni = parseInt(int.toString().split('').reverse().join(''));
+  // returning positive or negative reversed int
+  return isPositive ? tni : (tni * -1);
+}
 
 
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.substr(1))
+    .join(' ');
+}
 
 
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
-
+function maxCharacter(str) {
+  const charObj = {};
+  str.split('').forEach(char => {
+    charObj[char] ? charObj[char]++ : charObj[char] = 1;
+  });
+  let topVal = 0;
+  let topChar;
+  for(char in charObj) {
+    if(charObj[char] > topVal) {
+      topVal = charObj[char];
+      topChar = char;
+    }
+  }
+  return topChar
+}
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
-
+function fizzBuzz() {
+  for (i = 0; i < 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0 ) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 
 // Call Function
-const output = isPalindrome('hannah');
-
+const output = fizzBuzz();
 console.log(output);
