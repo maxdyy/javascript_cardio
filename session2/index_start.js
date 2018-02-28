e637b6b9aef33ac5ff691ec4998bc83975f88bc6
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2018-02-28 23:33:46 +0100

    First solution section 2

--- session2/index_start.js
+++ session2/index_start.js
@@ -6,6 +6,24 @@ function longestWord(sen) {
   // SOLUTION 1 - Return a single longest word
   // SOLUTION 2 - Return an array and include multiple words if they have the same length
   // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
+    // Create filtered array
+    const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+
+    // Sort by length
+    const sorted = wordArr.sort((a, b) => b.length - a.length);
+  
+    // If multiple words, put into array
+    const longestWordArr = sorted.filter(
+      word => word.length === sorted[0].length
+    );
+  
+    // Check if more than one array val
+    if (longestWordArr.length === 1) {
+      // Return the word
+      return longestWordArr[0];
+    } else {
+      return longestWordArr;
+    }
 }
 
 // CHALLENGE 2: ARRAY CHUNKING
